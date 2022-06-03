81d3d78192c44eeaa9257e06b103c5873821ed2a
sheikhamir007 <sheikhsadmanamir@gmail.com>
2022-06-03 12:29:43 -0400

    first day

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hello world ');
+// 1. Variable 
+// 2. operator
+// 3. data type
+// 4. loop 
+// 5. condition 
+// 6. function 
+// 7. array 
+
+// 8. Dom 
+
+var Name = "zubi";
+
+console.log(Name);
+
+function math() {
+    console.log('hello function');
+}
+math();
+
+
+var school = [ 'zubi' , 24 , 'bnaglka uu']; 
+console.log(school);
+
+
+
---
+++ test.js
@@ -0,0 +1,1 @@
+console.log('zubiar bin fedous')

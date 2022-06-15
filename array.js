eaaaf87c220a783cc2cf9b18f2aeb1570c71eef3
sheikhamir007 <sheikhsadmanamir@gmail.com>
2022-06-15 10:44:48 -0400

    ok

---
+++ array.js
@@ -0,0 +1,56 @@
+// var items = [ 'ball' , 'paper' , 'pen' , 'name' ,'bottle' , 'cow' , 'zubi' ];
+// console.log('1st',items);
+// var items2 = items.pop();
+// console.log(items2);
+// console.log(items);
+
+// var length = items.length; 
+// console.log( 'results' , length);
+
+// if(items.length < 5){
+//     console.log('ok')
+// }else{
+//     console.log('not ok')
+// }
+
+// for loop 
+// while loop 
+// do while loop 
+
+// var rostGiven = 0;
+// while(rostGiven < 5 ){
+//     rostGiven++;
+//     console.log( rostGiven, 'Rost den');
+// }
+
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// 
+
+
+// var position = items.indexOf('name');
+// console.log(position);
+// items.push('zubi');
+// console.log(items);
+// var position = items.indexOf('zubi');
+// console.log(position);
+// items.pop();
+// console.log(items);
+
+
+// var items = [ 'ball' , 'paper' , 'pen' , 'name' ];
+// var alldata = items.length;
+// console.log('result:' , alldata);
+
+// for(var i = 0; i < items.length; i++){
+//     var data = items[i]; 
+//     console.log(i, data);
+// }
+
+// 
+
+// 0123456789... 

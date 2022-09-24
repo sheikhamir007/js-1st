2ffae9e51cde26e0851b567a850c6cdc950aca09
sheikhamir007 <sheikhsadmanamir@gmail.com>
2022-09-23 23:03:52 -0400

    Java Script recap

---
+++ recap.js
@@ -0,0 +1,84 @@
+    // variable ( 3 types of )
+    // operator
+    // data type
+    // loop
+    // condition
+    // function
+    // array 
+    // dom
+
+    // var Name = "amir";
+    // console.log(Name);
+
+    // var school = 'bangladesh bank';
+    // console.log(school);
+
+    // var color = "pink";
+    // console.log(color);
+
+    // console.log('4 + 4');
+
+    // var user = 'amir';
+    // let school = 'amir school';
+    // const age = 24;
+
+    // console.log(user , school , age);
+
+    // function age(){
+    //     console.log('hello world zubi');
+    // }
+    // const myAge = age();
+    // console.log(myAge);
+
+
+    // srting 
+    // number 
+    // Boolean
+    // array 
+    // object -> last 
+
+    // var user = 'amir';
+    // var bool = true;
+    // var num = 40;
+    // var arr = ['zubi' , 'amir' , 'joy'];
+
+    // console.log(typeof(arr));
+
+    // var a = 10;
+    // var b = 20;
+    // var c = a+b;
+    // console.log(c);
+
+    // var give = 1;
+    // while( give < 10){
+    //     console.log( give , 'hello');
+    //     give++;
+    // }
+
+    // var taka = 11;
+
+    // if(taka > 20){
+    //     console.log('ice cream nibo');
+    // }else if(taka > 10){
+    //     console.log('take milk')
+    // }else{
+    //     console.log('go home');
+    // }
+
+    var Name = ['ami ' , 'tumi'  , 'tui' , 30 , true];
+    var value =  Name.length;
+
+    if ( 4 > value ) {
+        console.log('ok');
+    }else{
+        console.log('not ok')
+    }
+
+  
+
+
+    
+
+
+
+

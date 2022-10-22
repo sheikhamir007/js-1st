8c455e037821a07bf56a32530aac8c49b5237cb6
sheikhamir007 <sheikhsadmanamir@gmail.com>
2022-10-22 06:35:13 -0400

    JavaScript Let/Const introdcution

--- problem.js
+++ problem.js
@@ -1,18 +1,18 @@
 //  1 foot = 12 in
 
-function foot(data) {
-    result = data * 12;
-    return result;
-}
+// function foot(data) {
+//     result = data * 12;
+//     return result;
+// }
 
-var feet = 5;
-var resultOne = foot(feet);
-console.log('my inches', resultOne);
+// var feet = 5;
+// var resultOne = foot(feet);
+// console.log('my inches', resultOne);
 
 
-var data = 11;
-var result = data % 2;
-console.log(result)
+// var data = 11;
+// var result = data % 2;
+// console.log(result)
 
 // even and odd number 
 // 303 is it even or 
@@ -20,22 +20,38 @@ console.log(result)
 // 100 / 2 = 50 -> 0
 // 3 / 2 = 1 -> 1
 
-function evenOrOdd(number) {
+// function evenOrOdd(number) {
 
-    var test = number % 2;
+//     var test = number % 2;
 
-    if(test == 0){
-        return true;
-    }else{
-        return false;
-    }
+//     if(test == 0){
+//         return true;
+//     }else{
+//         return false;
+//     }
 
-}
+// }
 
-var num = 330;
-var oddEven = evenOrOdd(num);
-console.log(oddEven);
+// var num = 330;
+// var oddEven = evenOrOdd(num);
+// console.log(oddEven);
 
 // > < <= >= ==  , === , != 
 
 
+function max(number) {
+    let bigNum = number[0];
+    for(let x = 0; x < number.length; x++){
+        let index = x;
+        let data = number[index];
+        if(data > bigNum){
+            bigNum = data;
+        }
+        
+        return bigNum;
+    }
+}
+
+const value = [100 , 20 , 25 , 44 , 600];
+const bigdata = max(value);
+console.log(bigdata);
--- recapJS.js
+++ recapJS.js
@@ -1,64 +1,95 @@
 // 1 variable 
 
-var myName = 'amir';
-console.log(myName);
+// var myName = 'amir';
+// console.log(myName);
 
 // 2 array 
-var friend = ['abul' , 'babul' , 'ami' , 'tumi'];
+// var friend = ['abul' , 'babul' , 'ami' , 'tumi'];
 
-var friendName = friend[2];
-var frLen = friend.length; 
-var indexOf = friend.indexOf('babul');
+// var friendName = friend[2];
+// var frLen = friend.length; 
+// var indexOf = friend.indexOf('babul');
 
-console.log(friendName);
-console.log(frLen);
-console.log(indexOf);
+// console.log(friendName);
+// console.log(frLen);
+// console.log(indexOf);
 
 // 3 condition  
 
-if (frLen < 3) {
-    console.log('true');
-}else if(frLen <= 4){
-    console.log('true 2nd');
-}
-else{
-    console.log('false')
-}
+// if (frLen < 3) {
+//     console.log('true');
+// }else if(frLen <= 4){
+//     console.log('true 2nd');
+// }
+// else{
+//     console.log('false')
+// }
 
-// loop 
-var num = 1;
-while(num <= 6){
-    console.log( num ,'hello');
-    num++
-}
+// // loop 
+// var num = 1;
+// while(num <= 6){
+//     console.log( num ,'hello');
+//     num++
+// }
 
-for(var i = 1 ; i < 6 ; i++){
-    console.log( i ,'hello world');
-}
+// for(var i = 1 ; i < 6 ; i++){
+//     console.log( i ,'hello world');
+// }
 
 // function
 
-function age(data){
-    if(data > 20){
-       return 'ready for marriage'
-    }else{
-        return ' chill '
-    }
-}
+// function age(data){
+//     if(data > 20){
+//        return 'ready for marriage'
+//     }else{
+//         return ' chill '
+//     }
+// }
 
 // object 
 
-var amir = {
-    age : 25,
-    data : 'study',
-    color : 'pink',
-    car : 'nissan'
-}
+// var amir = {
+//     age : 25,
+//     data : 'study',
+//     color : 'pink',
+//     car : 'nissan'
+// }
 
 // console.log(amir.car);
-var ready = amir.age ;
-var result = age(ready);
-console.log(result)
+// var ready = amir.age ;
+// var result = age(ready);
+// console.log(result)
 
 // math  + - * / % 
 // shothand ++ , -- ,
+
+// == vs === 
+
+// var data = 10 ;
+// var data2 = 10;
+
+// if (data === data2){
+//     console.log('same');
+// }else{
+//     console.log('not same')
+// }
+
+var data = 'amir'; 
+data = 'zubi';
+console.log(data);
+
+let data2 = 'amir';
+data2  = 'amir developer';
+console.log(data2);
+
+const countryName  = 'bangladesh valo na ';
+// countryName = 'usa';
+console.log(countryName)
+
+// leap year 
+// max value 
+// factorial 
+
+
+ 
+
